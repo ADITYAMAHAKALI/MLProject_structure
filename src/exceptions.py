c06115bf6fcf5f74d6c5247379460c5e6d4c2060
adityamahakali <adityamahakali@gmail.com>
2023-07-10 22:31:52 +0530

    project structure, logging, exception

---
+++ src/exceptions.py
@@ -0,0 +1,14 @@
+import sys 
+
+def error_details(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    error_message = f"error occured in python file {exc_tb.tb_frame.f_code.co_filename} at line {exc_tb.tb_lineno}, error = {error}"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_details(error=error_message,error_detail=error_detail)
+        
+    def __str__(self) -> str:
+        return self.error_message
